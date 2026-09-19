e34f1fa47c5240ee1b5fe8ec0c27cc773a62d53e
Federica Palumbo <federicapalumbo10@gmail.com>
2026-09-20 00:13:40 +0200

    aggiunto commento esplicativo in file operation log entity

--- src/api/operation-log/operation-log.entity.ts
+++ src/api/operation-log/operation-log.entity.ts
@@ -1,3 +1,4 @@
+//questo file è la "forma" del dato: dice che ogni riga del registro avrà un tipo di operazione, un IP, una data e un esito.
 import { Types } from "mongoose";
 
 export type TipoOperazione = 'Login' | 'Ricarica' | 'Bonifico' | 'CambioPassword';
